be74eb5940b5be6cff34c49efa0b9917bc433fbd
mary-13 <marinakubrina@mail.ru>
2017-10-10 20:24:52 +0600

    Fill form

---
+++ converter/converter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select file with extention *.xls or *.xlsx";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(50, 52);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(108, 27);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select file";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Location = new System.Drawing.Point(50, 121);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(108, 27);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert to *.mkb";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Enabled = false;
+            this.btnOpen.Location = new System.Drawing.Point(50, 154);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(108, 27);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open converted file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(50, 187);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(108, 27);
+            this.btnInfo.TabIndex = 6;
+            this.btnInfo.Text = "How it works?";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 221);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MKB-conveter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnInfo;
+    }
+}
+
---
+++ converter/converter/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            Stream stream = null;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "D:\\";
+            dialog.Filter = "Excel files (*.xls, *.xlsx, *.xlsm)|*.xls;*.xlsx;*.xlsm";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    /*excelappworkbook = excelapp.Workbooks.Open(dialog.FileName,
+    Type.Missing, Type.Missing, "   ", Type.Missing,
+    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+    Type.Missing, Type.Missing);
+                    excelsheets = excelappworkbook.Worksheets;
+                    //Получаем ссылку на лист 1
+                    excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);*/
+                    // здесь будет код по открытию эксель-файла :)
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Файл не выбран ");
+                }
+            string name = dialog.FileName;
+            int position = name.LastIndexOf("\\");
+            name = name.Substring(position + 1);
+            label2.Text = "Selected file:";
+            label3.Text = name;
+            btnConvert.Enabled = true;
+            btnSelect.Enabled = false;
+            btnOpen.Enabled = false;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // здесь будет код по конвертированию файла :)
+            MessageBox.Show("Типо готово");
+
+            label2.Text = "File converted!";
+            label3.Text = "";
+            btnSelect.Enabled = true;
+            btnOpen.Enabled = true;
+            btnConvert.Enabled = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // здесь будет код по открытию файла
+            MessageBox.Show("Типо файл открылся");
+
+            label2.Text = "";
+            btnSelect.Enabled = true;
+            btnOpen.Enabled = false;
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To change the format from *.xls or *.xlsx to *.mkb, you need to save the sheet called \"Expert\" and open the Excel file by clicking the \"Select file\" button.\n\nAfter that the \"Convert to *.mkb\" button will become active. To convert the file, click this button and wait for the file to be processed.\n\nAt the end of the process, you can open a file or select a new one for conversion.\n\nPath to the converted file: \"D:\\Knowledge base\"\n\nThe program is developed by Marina Kubrina(c). 2017");
+        }
+    }
+}
